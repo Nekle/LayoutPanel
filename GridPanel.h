c2807f771b61cd3636f60f374fddcf2fd66887e7
nekle <nekleding@gmail.com>
2013-10-12 22:36:06 +0800

    update grid layout

--- GridPanel.h
+++ GridPanel.h
@@ -8,21 +8,60 @@
 
 #import <UIKit/UIKit.h>
 
+#import "StackPanel.h"
 
 /*
  * 九宫格 布局方式
  */
 
-typedef struct _GridPanelMap
+#define HighByte(t) ((t)&0xFF00)
+#define LowByte(t) ((t)&0x00FF)
+
+typedef enum _GridLayout
 {
-    int **map;
-}GridPanelMap;
+    eLayoutLeftTop = 0x00,
+    eLayoutLeftCenter = 0x01,
+    eLayoutLeftBottom = 0x02,
+    eLayoutCenterTop = 0x03,
+    eLayoutCenterCenter = 0x04,
+    eLayoutCenterBottom = 0x05,
+    eLayoutRightTop = 0x06,
+    eLayoutRightCenter = 0x07,
+    eLayoutRightBottom = 0x08,
+    
+    eLayoutFillWidth  = 0x4000,
+    eLayoutFillHeight = 0x8000,
+    
+} GridLayoutArchor;
+
+@interface CRect : NSObject
+@property (nonatomic, assign)    CGPoint origin;
+@property (nonatomic, assign)    CGSize size;
+- (NSString*) description;
+- (CGRect) getCGRect;
+@end
+
+
+
+CRect* CRectMake(float x, float y, float width, float height) ;
+#define CRectZero CRectMake(0, 0, 0, 0)
+
+
+#define kGridPanelMaxElements 20
 
 @interface GridPanel : UIView
+{
+    NSMutableArray * rowHeightDefines;
+    NSMutableArray * columnWidthDefines;
+    NSMutableArray * archorRects;
+    NSMutableArray * margins;
+    int elementLayoutTypes[kGridPanelMaxElements];
+}
 
-@property (nonatomic, assign, readonly) int columns;
-@property (nonatomic, assign, readonly) int rows;
 @property (nonatomic, retain, readonly) NSMutableArray *elements;
-@property (nonatomic, assign, readonly) GridPanelMap gridPanelMap;
 
+//-(void)setRows:(int)rs andColumns:(int)cs;
+- (void) setRows:(NSMutableArray*)rs andColumn:(NSMutableArray*)cs;
+-(void)addView:(UIView*)element atRow:(int)row andColumn:(int)column withMargin:(LayoutMargin*)margin archor:(GridLayoutArchor) archor;
+- (void)updateLayout;
 @end
--- GridPanel.m
+++ GridPanel.m
@@ -8,21 +8,47 @@
 
 #import "GridPanel.h"
 
-@implementation GridPanel
+@implementation CRect
+@synthesize size;
+@synthesize origin;
+-(NSString *)description
 {
-    CGSize elementSize;
+    return [NSString stringWithFormat:@"[x=%6.2f, y=%6.2f, w=%6.2f, h=%6.2f]", origin.x, origin.y, size.width, size.height];
+}
+- (CGRect)getCGRect
+{
+    return CGRectMake(origin.x, origin.y, size.width, size.height);
+}
+@end
+
+CRect * CRectMake(float x, float y, float width, float height)
+{
+    CRect * rect = [[[CRect alloc] init] autorelease];
+    CGPoint origin = CGPointMake(x, y);
+    CGSize size = CGSizeMake(width, height);
+    rect.size = size;
+    rect.origin = origin;
+    
+    return rect;
 }
 
-@synthesize rows;
-@synthesize columns;
+
+@implementation GridPanel
+{
+    
+}
+
 @synthesize elements;
-@synthesize gridPanelMap;
 
 
 -(void)dealloc
 {
     [elements release];
-     free(gridPanelMap.map);
+    [archorRects release];
+    [rowHeightDefines release];
+    [columnWidthDefines release];
+    [margins release];
+    
     [super dealloc];
 }
 
@@ -31,11 +57,6 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        rows = 1;
-        columns = 1;
-        elementSize = CGSizeMake(frame.size.width, frame.size.height);
-        gridPanelMap.map = malloc(rows*columns * sizeof(int));
-        memset(gridPanelMap.map, 0, rows*columns*sizeof(int));
     }
     return self;
 }
@@ -50,43 +71,240 @@
 */
 
 
--(CGPoint)getOriginPointOfRow:(int)row column:(int)column
+-(void)setFrame:(CGRect)frame
 {
-    CGRect frame = self.frame;
-    CGPoint origin = CGPointZero;
-    origin.x = column / (CGFloat)columns * frame.size.width;
-    origin.y = row / (CGFloat)rows * frame.size.height;
+    [super setFrame:frame];
     
-    gridPanelMap.map = realloc(gridPanelMap.map, rows*columns*sizeof(int));
-    
-    return  origin;
+    [self updateLayout];
 }
 
--(void)setRows:(int)rs andColumns:(int)cs
+
+// ["12%", "12"]
+
+- (float) str:(NSString*) str valueOf:(float) total
 {
-    if (rs <= 0 || cs <= 0) {
-        NSLog(@"GridPanel row or column can't <= 0!");
-        return ;
+    float length = [str floatValue];
+    if ([str hasSuffix:@"%"]) {
+        length = length / 100 * total;
     }
-    rows = rs;
-    columns = cs;
-    elementSize = CGSizeMake(self.frame.size.width/rows, self.frame.size.height/columns);
+    
+    return length;
 }
 
--(void)addView:(UIView*)element atRow:(int)row column:(int)column
+- (void) updateArchorRect
 {
-    if (row<0 || column<0 || row >=rows || column>=columns ) {
-        NSLog(@"row  must in [0, %d) and column must in [0, %d)", rows, columns);
+    CGPoint origin = CGPointMake(0, 0);
+    
+    if (archorRects == nil) {
+        archorRects = [[NSMutableArray alloc] init];
+    } else {
+        [archorRects removeAllObjects];
+    }
+    
+    for (int i = 0; i < rowHeightDefines.count; i++) {
+
+        NSString *row = [rowHeightDefines objectAtIndex:i];
+        float height = [self str:row valueOf:self.frame.size.height];
+        
+        origin.x = 0;
+        for (int j = 0; j < columnWidthDefines.count; j++) {
+            NSString *column = [columnWidthDefines objectAtIndex:j];
+            float width = [self str:column valueOf:self.frame.size.width];
+            [archorRects addObject: CRectMake(origin.x, origin.y, width, height)];
+            origin.x += width;
+        }
+        
+        origin.y += height;
+    }
+}
+
+- (void) setRows:(NSMutableArray*)rs andColumn:(NSMutableArray*)cs
+{
+    if (rs.count * cs.count > kGridPanelMaxElements) {
+        NSLog(@"element max then [ kGridPanelMaxElements = %d], you can modify kGridPanelMaxElements define to satisfy you need", kGridPanelMaxElements);
         return;
     }
     
-    if (elements) {
-        elements = [[NSMutableArray alloc] init];
+    [rs retain];
+    [rowHeightDefines release];
+    rowHeightDefines = rs;
+    
+    [cs retain];
+    [columnWidthDefines release];
+    columnWidthDefines = cs;
+    
+    [self updateArchorRect];
+}
+
+
+-(CGRect)getArchorRectOfRow:(int)row column:(int)column
+{
+    if (row >= rowHeightDefines.count || column >= columnWidthDefines.count ) {
+        NSLog(@"error row or column [%d, %d]", row, column);
+        return CGRectMake(0, 0, 0, 0);
+    }
+    int idx = column + row * columnWidthDefines.count;
+    
+    if (idx >= archorRects.count)
+    {
+        NSLog(@"error row or column [%d, %d]", row, column);
+        return CGRectMake(0, 0, 0, 0);
     }
     
-    [elements addObject:element];
-    gridPanelMap.map[row][column] = 1;
+    CRect *r = archorRects[idx];
+        
+    return  [r getCGRect];
 }
+
+//- (void)layout:(CGRect*)frame leftOf
+
+
+
+- (void)layout:(UIView*)element in:(CGRect)archorRect withMargin:(LayoutMargin*)margin andArchor:(GridLayoutArchor) archor
+{
+    
+    GridLayoutArchor fill = HighByte(archor);
+    if (fill & eLayoutFillWidth) {
+        CGRect frame = element.frame;
+        float width = archorRect.size.width - (margin.left + margin.right);
+        frame.size.width = width < 0?0:width;
+        element.frame = frame;
+    }
+    
+    if (fill & eLayoutFillHeight) {
+        CGRect frame = element.frame;
+        float height = archorRect.size.height - (margin.top + margin.bottom);
+        frame.size.height = height < 0? 0: height;
+        element.frame = frame;
+    }
+    
+    GridLayoutArchor layout = LowByte(archor);
+    
+    switch (layout) {
+        case eLayoutLeftTop:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + margin.left;
+                frame.origin.y = archorRect.origin.y + margin.top;
+                element.frame = frame;
+            }
+            break;
+        case eLayoutLeftCenter:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + margin.left;
+                frame.origin.y = archorRect.origin.y + archorRect.size.height/2 - frame.size.height/2;
+                element.frame = frame;
+            }
+            break;
+        case eLayoutLeftBottom:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + margin.left;
+                frame.origin.y = archorRect.origin.y + archorRect.size.height - (frame.size.height + margin.bottom);
+                element.frame = frame;
+            }
+            break;
+        case eLayoutCenterTop:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + archorRect.size.width/2 - frame.size.width/2;
+                frame.origin.y = archorRect.origin.y + margin.top;
+                element.frame = frame;
+            }
+            break;
+        case eLayoutCenterCenter:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + archorRect.size.width/2 - frame.size.width/2;
+                frame.origin.y = archorRect.origin.y + archorRect.size.height/2 - frame.size.height/2;
+                element.frame = frame;
+            }
+            break;
+        case eLayoutCenterBottom:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + archorRect.size.width/2 - frame.size.width/2;
+                frame.origin.y = archorRect.origin.y + archorRect.size.height - (frame.size.height + margin.bottom);
+                element.frame = frame;
+            }
+            break;
+        case eLayoutRightTop:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + archorRect.size.width - (frame.size.width + margin.right);
+                frame.origin.y = archorRect.origin.y + margin.top;
+                element.frame = frame;
+            }
+            break;
+        case eLayoutRightCenter:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + archorRect.size.width - (frame.size.width + margin.right);
+                frame.origin.y = archorRect.origin.y + archorRect.size.height/2 - frame.size.height/2;
+                element.frame = frame;
+            }
+            break;
+        case eLayoutRightBottom:
+            {
+                CGRect frame = element.frame;
+                frame.origin.x = archorRect.origin.x + archorRect.size.width - (frame.size.width + margin.right);
+                frame.origin.y = archorRect.origin.y + archorRect.size.height - (frame.size.height + margin.bottom);
+                element.frame = frame;
+            }
+            break;
+        default:
+            break;
+    }
+
+}
+
+-(void)addView:(UIView*)element atRow:(int)row andColumn:(int)column withMargin:(LayoutMargin *)margin archor:(GridLayoutArchor)archor
+{
+    if (row >= rowHeightDefines.count || column >= columnWidthDefines.count) {
+        NSLog(@"row or column error!");
+        return;
+    }
+    
+    // element
+    if (elements == nil) {
+        elements = [[NSMutableArray alloc] init];
+    }
+    [elements addObject:element];
+    
+    // margin
+    if (margins == nil) {
+        margins = [[NSMutableArray alloc] init];
+    }
+    [margins addObject:margin];
+    
+    // layoutType
+    elementLayoutTypes[elements.count - 1] = archor;
+    
+    [self addSubview:element];
+    //CGRect frame = element.frame;
+    
+    CGRect archorRect = [self getArchorRectOfRow:row column:column];
+    
+    [self layout:element in:archorRect withMargin:margin andArchor:archor];
+}
+
+- (void)updateLayout
+{
+    [self updateArchorRect];
+    
+    for (int i =0 ; i < rowHeightDefines.count; i++){
+        for (int j = 0; j < columnWidthDefines.count; j++) {
+            CGRect archorRect = [self getArchorRectOfRow:i column:j];
+            int idx = i*columnWidthDefines.count+j;
+            UIView * view = [elements objectAtIndex: idx ];
+            LayoutMargin * margin = [margins objectAtIndex:idx];
+            GridLayoutArchor archor = elementLayoutTypes [idx];
+            [self layout:view in:archorRect withMargin: margin andArchor:archor];
+        }
+    }
+}
+
 
 
 @end
